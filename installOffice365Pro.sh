a72c2e4a7ad7ba1c49166285eeae05bade53c531
Shivanand Anchalkar <snandaworld@hotmail.com>
2024-09-18 11:58:00 +0530

    Update installOffice365Pro.sh

--- installOffice365Pro.sh
+++ installOffice365Pro.sh
@@ -32,7 +32,7 @@ tempfile="$appname.pkg"
 log="$logandmetadir/$appname.log"                                               # The location of the script log file
 metafile="$logandmetadir/$appname.meta"                                         # The location of our meta file (for updates)
 BACKUPPATH="~/Documents/Outlook_Backup"
-SOURCEPATH="~/Library/Group Containers/UBF8T346G9.Office/Outlook/"
+SOURCEPATH="~/Library/Group Containers/UBF8T346G9.Office/Outlook"
 # function to delay script if the specified process is running
 waitForProcess () {
 
